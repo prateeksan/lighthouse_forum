ba0881ebe54f163db21ab83edd12fedd4ee82ec1
vagrant <vagrant@lighthouse-labs>
2015-11-17 02:43:52 +0000

    finished lhl forum

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LighthouseForumCd::Application.config.secret_key_base = '8298e6340967dd780432dead2028564e3b31d4e98f81e8eed55e8c2a222396ec371b2a641db84ca12181282293c1b70723b50a765966c593b57c94ee5a283f46'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LighthouseForumCd::Application.config.session_store :cookie_store, key: '_lighthouse_forum_cd_session'
